92cf07b36fa91a277b94e0730696e9f2417926d6
Sachin Rajput <sachin.rajput.k@gmail.com>
2023-04-26 14:53:00 -0400

    Add token usage display

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from services.template_service import TemplateService
 from services.chain_service import ChainService
 from models.openai import OpenAI
+from langchain.callbacks import get_openai_callback
 
 template = """ 
 Question: {question}
@@ -10,10 +11,13 @@ promptTemplate = TemplateService().create_template(template)
 chain = ChainService(OpenAI()).get_chain(promptTemplate)
 
 while True:
-    question = input("Ask me anything!\n")
+    with get_openai_callback() as cb:
+        question = input("Ask me anything!\n")
 
-    if question.lower() == "quit":
-        break
+        if question.lower() == "quit":
+            break
 
-    print(chain.run(question))
+        print(chain.run(question))
+        print('\n')
+    print(cb)
     print('\n')
